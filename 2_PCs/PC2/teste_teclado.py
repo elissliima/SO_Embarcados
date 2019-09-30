52c1d38861d04d0a75c402d24ff43f47fb4c9a7f
Elisa <eliss.liima@gmail.com>
2019-09-30 10:00:14 -0300

    add novo teste

--- 2_PCs/PC2/teste_teclado.py
+++ 2_PCs/PC2/teste_teclado.py
@@ -6,31 +6,31 @@ MATRIX = [[1,2,3,'A'],
           [7,8,9,'C'],
           ['*',0,'#','D']]
           
- ROW = [7,11,13,15]
- COL = [12,16,18,22]
+ROW = [7,11,13,15]
+COL = [12,16,18,22]
  
- for j in range(4):
+for j in range(4):
         GPIO.setup(COL[j],GPIO.OUT)
         GPIO.output(COL[J],1)
         
- for i in range (4);
+for i in range (4):
         GPIO.setup(ROW (i),GPIO.IN, pull_up_down =GPIO.PUD_UP)
         
         
-  try:
+try:
         while(True):
                  for j in range(4):
                           GPIO.output(COL[j],0)
                           
-                          for i in range (4):
-                                 if GPIO.imput(ROW[i]) == 0;
-                                         print MATRIX[i][j]
-                                         while(GPIO.input(ROW[i] ==0);
-                                                pass
+                           for i in range (4):
+                               	  if GPIO.imput(ROW[i]) == 0:
+                                       	  print MATRIX[i][j]
+                                       	  while(GPIO.input(ROW[i] ==0)):
+                                         	  pass
                            
-                           GPIO.output(COL[J],1)
+                           GPIO.output(COL[j],1)
         
-   except:KeyboardInterrupt:
+except:KeyboardInterrupt:
           GPIO.cleanup()
    
         
